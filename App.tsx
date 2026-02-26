6e68e6ca3402f04c26a2f715c7b54331d05dccdb
Huy04@ <he182181nguyenquanghuy@gmail.com>
2026-02-26 15:40:08 +0700

    Revert "Qhuy"

--- App.tsx
+++ App.tsx
@@ -1,32 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './pages/Login';
-import Home from './pages/Home';
-import Layout from './components/Layout';
-
-// Protected Route
-const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
-    const user = localStorage.getItem('user');
-    return user ? <>{children}</> : <Navigate to="/login" />;
-};
 
 const App = () => {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/login" element={<Login />} />
-                <Route
-                    path="/"
-                    element={
-                        <ProtectedRoute>
-                            <Layout>
-                                <Home />
-                            </Layout>
-                        </ProtectedRoute>
-                    }
-                />
-            </Routes>
-        </BrowserRouter>
+        <div className="flex flex-col min-h-screen items-center justify-center">
+            <h1 className="text-3xl font-bold text-primary">Welcome to Foodly</h1>
+        </div>
     );
 };
 
